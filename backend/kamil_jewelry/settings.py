c0b88a74d34c66dc2bdbe5bfe49028b56c3ef67e
Bekkhan <87779571856b@gmail.com>
2025-09-03 20:20:20 +0500

    Fix Docker Compose compatibility issues and add multiple versions

--- backend/kamil_jewelry/settings.py
+++ backend/kamil_jewelry/settings.py
@@ -133,6 +133,10 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
+    "http://taxi.aktau-go.kz:3001",
+    "http://taxi.aktau-go.kz:3000",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
