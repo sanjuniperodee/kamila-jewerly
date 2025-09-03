c1e06d6c66ba12654b00838e465aad55a98aa96f
Bekkhan <87779571856b@gmail.com>
2025-09-03 20:40:18 +0500

    Fix Docker Compose compatibility issues and add multiple versions

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', '127.0.0.1'],
+    domains: ['localhost', '127.0.0.1', 'taxi.aktau-go.kz'],
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '8000',
+        pathname: '/media/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'taxi.aktau-go.kz',
+        port: '8017',
         pathname: '/media/**',
       },
     ],
