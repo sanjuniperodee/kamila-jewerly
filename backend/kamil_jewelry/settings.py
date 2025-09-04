b50259085eb3d414946e33b9903ef72ea5a11e9d
Bekkhan <87779571856b@gmail.com>
2025-09-04 14:23:31 +0500

    f

--- backend/kamil_jewelry/settings.py
+++ backend/kamil_jewelry/settings.py
@@ -133,6 +133,10 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "https://kamiljewelry.kz",
     "https://www.kamiljewelry.kz",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
