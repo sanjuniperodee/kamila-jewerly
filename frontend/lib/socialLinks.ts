c2334755759f6034cf4355b5b43f1d1f75a2f03a
Bekkhan <87779571856b@gmail.com>
2025-09-04 16:10:18 +0500

    f

---
+++ frontend/lib/socialLinks.ts
@@ -0,0 +1,17 @@
+export const socialLinks = {
+  instagram: 'https://instagram.com/kamiljewelry',
+  whatsapp: 'https://wa.me/77051293500',
+  telegram: 'https://t.me/kamiljewelry',
+  vk: 'https://vk.com/kamiljewelry'
+}
+
+export const contactInfo = {
+  phone: '+7 (705) 129-00-00',
+  email: 'info@kamiljewelry.kz',
+  address: 'г. Астана, ТЦ Жаннур, Проспект Абая, 48, 1 этаж',
+  workingHours: {
+    weekdays: '10:00 - 22:00',
+    saturday: '10:00 - 22:00',
+    sunday: '10:00 - 22:00'
+  }
+}
