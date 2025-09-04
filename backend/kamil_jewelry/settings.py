c25c46fbdb58f41eae56c5dc6781c27e5f2b725d
Bekkhan <87779571856b@gmail.com>
2025-09-04 15:24:28 +0500

    f

--- backend/kamil_jewelry/settings.py
+++ backend/kamil_jewelry/settings.py
@@ -137,6 +137,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "https://kamila-jewerly-h295uavu1-bekkhans-projects.vercel.app/",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
